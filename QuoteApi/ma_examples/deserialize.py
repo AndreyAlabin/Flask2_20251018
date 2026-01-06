dea83b40d8193a1db42fcc18ffd1b949b3af539f
AndreyAlabin <dirdep@outlook.com>
2026-01-06 12:43:28 -0800

    Correct email

--- QuoteApi/ma_examples/deserialize.py
+++ QuoteApi/ma_examples/deserialize.py
@@ -6,18 +6,18 @@ json_data = """
 {
     "id": 12,
     "name": "Ivan",
-    "email": "ivanmail.ru",
+    "email": "ivan@mail.ru",
     "age": "21"
 }
 """
 
 try:
-    schema = AuthorSchema(partial=False, unknown=EXCLUDE)
+    schema = AuthorSchema(partial=False)  # unknown=EXCLUDE
     result = schema.loads(json_data, unknown=EXCLUDE)
     print(type(result), result)
 except ValidationError as e:
     print(e.messages)
 
-# r = schema.load(result)
-# print(type(r), r)
-# print(Author(**r))
+r = schema.load(result, unknown=EXCLUDE)
+print(type(r), r)
+print(Author(**r))
