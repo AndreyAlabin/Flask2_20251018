c4adbcd40eaabf752733c905f4e74179d5f1fcbc
AndreyAlabin <dirdep@outlook.com>
2026-01-06 13:46:04 -0800

    Done practice with many

--- QuoteApi/ma_examples/deserialize.py
+++ QuoteApi/ma_examples/deserialize.py
@@ -1,6 +1,7 @@
 from marshmallow import EXCLUDE, INCLUDE, RAISE, ValidationError
 from author import Author
 from schema import AuthorSchema
+from pprint import pprint
 
 json_data = """
 {
@@ -20,3 +21,33 @@ except ValidationError as e:
 r = schema.load(result, unknown=INCLUDE) # unknown=EXCLUDE
 print(type(r), r)
 print(Author(**r))
+
+json_data_list = """
+[
+   {
+       "id": 1,
+       "name": "Alex",
+       "email": "alex@mail.ru"
+   },
+   {
+       "id": 2,
+       "name": "Ivan",
+       "email": "ivan@mail.ru"
+   },
+   {
+       "id": 4,
+       "name": "Tom",
+       "email": "tom@mail.ru"
+   }
+]
+"""
+
+authors_schema = AuthorSchema(many=True)
+res_one = authors_schema.loads(json_data_list, unknown=INCLUDE)
+# print(f'{res_one = !r}')
+pprint(res_one)
+
+# либо так
+
+res_one = authors_schema.loads(json_data_list, many=True, unknown=INCLUDE)
+pprint(res_one, sort_dicts=False)
--- QuoteApi/ma_examples/serialize.py
+++ QuoteApi/ma_examples/serialize.py
@@ -1,7 +1,24 @@
 from author import Author
 from schema import AuthorSchema
+from pprint import pprint
 
 author = Author(1, "Alex", "alex5@mail.ru")
 author_schema = AuthorSchema()
 result = author_schema.dump(author)
 print(type(result), result)
+
+
+authors_list = [
+   Author("1", "Alex"),
+   Author("1", "Ivan"),
+   Author("1", "Tom")
+]
+
+authors_schema = AuthorSchema(many=True)
+result = authors_schema.dump(authors_list)  # dump / dumps
+pprint(result, sort_dicts=False)
+
+# либо так
+
+result = authors_schema.dump(authors_list, many=True)
+pprint(repr(result))
