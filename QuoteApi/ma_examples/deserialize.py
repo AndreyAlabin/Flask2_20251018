bcdd82880393d758c5aafe6ef17fcced2e024ccc
AndreyAlabin <dirdep@outlook.com>
2026-01-06 13:02:53 -0800

    Add dump_only example

--- QuoteApi/ma_examples/deserialize.py
+++ QuoteApi/ma_examples/deserialize.py
@@ -4,20 +4,19 @@ from schema import AuthorSchema
 
 json_data = """
 {
-    "id": 12,
+    "id": 2,
     "name": "Ivan",
-    "email": "ivan@mail.ru",
-    "age": "21"
+    "email": "ivan@mail.ru"
 }
 """
 
 try:
     schema = AuthorSchema(partial=False)  # unknown=EXCLUDE
-    result = schema.loads(json_data, unknown=EXCLUDE)
+    result = schema.loads(json_data, unknown=INCLUDE) # unknown=EXCLUDE
     print(type(result), result)
 except ValidationError as e:
     print(e.messages)
 
-r = schema.load(result, unknown=EXCLUDE)
+r = schema.load(result, unknown=INCLUDE) # unknown=EXCLUDE
 print(type(r), r)
 print(Author(**r))
--- QuoteApi/ma_examples/schema.py
+++ QuoteApi/ma_examples/schema.py
@@ -1,7 +1,7 @@
 from marshmallow import Schema, fields
 
 class AuthorSchema(Schema):
-    id = fields.Integer(strict=True)
+    id = fields.Integer(required=True, strict=True, dump_only=True)
     name = fields.String(required=True, error_messages={"required": "Please enter a name"})
     surname = fields.String(required=False)
     email = fields.Email(required=True, error_messages={"required": "Please enter a email"})
